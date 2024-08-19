97ccfbd7b8a4197b534481a16203d8f20f50633a
Anamika <anamikadobriyal21@gmail.com>
2024-08-20 00:38:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function getFormvalue() {
-    //Write your code here
+// function getFormvalue() {
+//     //Write your code here
 
+// }
+
+function getFormValue(event) {
+    event.preventDefault();
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    alert(`${firstName} ${lastName}`);
 }
+
+document.getElementById('loginForm').addEventListener('submit', getFormValue);
