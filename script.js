b276aea93d8f6534a39d5139728d923ca2c2ebd8
Anamika <anamikadobriyal21@gmail.com>
2024-08-27 19:37:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 //     //Write your code here
 
 // }
-
-function getFormValue(event) {
-    event.preventDefault();
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    alert(`${firstName} ${lastName}`);
+function getFormvalue(event) {
+	event.preventDefault();
+    // Get the first name value
+    // let firstName = document.getElementById('fname').value;
+    
+    // // Get the last name value
+    // let lastName = document.getElementById('lname').value;
+	let firstName = document.getElementsByName('fname')[0].value;
+    let lastName = document.getElementsByName('lname')[0].value;
+    
+    // Concatenate first and last name
+    let fullName = firstName + " " + lastName;
+    
+    // Display the full name using an alert
+    alert(fullName);
 }
-
-document.getElementById('loginForm').addEventListener('submit', getFormValue);
